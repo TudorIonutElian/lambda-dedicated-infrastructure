3989a2761cbf83ede3a1bdb684b130dec29cb4fd
Tudor Ionut - Elian <ionuteliantudor@gmail.com>
2024-10-17 15:41:10 +0300

    Create data-archieve.tf

---
+++ infrastructure/data-archieve.tf
@@ -0,0 +1,14 @@
+/*
+  * data "archive_file" is used to create a zip file from the source file.
+  * The source file is the index.js file in the lambda directory.
+  * The output path is the lambda_function_payload.zip file.
+   - type: The type of archive to create (zip in this case).
+    - source_file: The path to the file or directory to be archived.
+    - output_path: The path to save the archive file.
+*/
+
+data "archive_file" "lambda_push_metrics_vpc_endpoint_func_archive" {
+  type        = "zip"
+  source_file = "../lambda/lambda-dedicated-infrastructure/index.js"
+  output_path = "lambda_dedicated_infrastructure_func.zip"
+}
