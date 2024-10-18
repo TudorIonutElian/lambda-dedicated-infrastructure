ad63311d435592cc944fa794ad79ee5cc16980f6
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-10-18 09:44:33 +0300

    Architecture -- cleany

--- infrastructure/iam_policies_attachment.tf
+++ infrastructure/iam_policies_attachment.tf
@@ -9,11 +9,3 @@ resource "aws_iam_role_policy_attachment" "terraform_lambda_policy" {
   role       = aws_iam_role.lambda_dedicated_infrastructure_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
-
-/*
-  aws_iam_role_policy_attachment is a resource that attaches an IAM policy to an IAM role.
-  In this case, the LambdaPutMetricPolicy policy is attached to the IAM role created earlier.
-  This policy allows the Lambda function to put custom metrics to CloudWatch.
-  - role: The name of the IAM role to attach the policy to.
-  - policy_arn: The ARN of the IAM policy to attach to the role.
-*/
