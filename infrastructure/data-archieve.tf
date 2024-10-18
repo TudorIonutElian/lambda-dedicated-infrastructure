a054253744226c62413da97c47e73c671c9db8d8
Tudor Ionut - Elian <ionuteliantudor@gmail.com>
2024-10-18 10:11:27 +0300

    Update data-archieve.tf

--- infrastructure/data-archieve.tf
+++ infrastructure/data-archieve.tf
@@ -12,3 +12,4 @@ data "archive_file" "lambda_dedicated_infrastructure_archive" {
   source_file = "${path.module}/../lambda/index.js"
   output_path = "${path.module}/lambda_dedicated_infrastructure_func.zip"
 }
+
